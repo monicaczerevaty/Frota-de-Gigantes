0b212e753f2b19316be36b44af3d4cd128b8a9eb
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-10-24 08:48:17 -0300

    4567

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-fetch('https://api.exemplo.com/dados')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Rede não está disponível!');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data); // Exibe os dados no console
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-    });
+import {style} from style
